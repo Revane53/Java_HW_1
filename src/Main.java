37c7a35a6537e4e8212d56f42733d4e93c6275e4
Margarin <margarin@student.21-school.ru>
2024-05-21 06:43:11 +0300

    add exercise 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /*============= exercise 1 =============*/
+        System.out.print("============= exercise 1 =============\n");
+        System.out.print("""
+                "Your time is limited,
+                  so don't waste if
+                      living someone else's life"
+                          Steve Jobs
+                """);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        /*============= exercise 2 =============*/
+        System.out.print("============= exercise 2 =============\n");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number\n");
+        float a = in.nextFloat();
+        System.out.print("Enter percent\n");
+        float b = in.nextFloat();
+        System.out.printf("%f percent of %f = %f\n", b, a, b/100*a);
+
+        /*============= exercise 3 =============*/
+        System.out.print("============= exercise 3 =============\n");
+        System.out.print("Enter number one\n");
+        int num_1 = in.nextInt();
+        System.out.print("Enter number two\n");
+        int num_2 = in.nextInt();
+        System.out.print("Enter number three\n");
+        int num_3 = in.nextInt();
+        System.out.printf("Your number = %d\n", num_1*100 + num_2*10 + num_3);
+
+        /*============= exercise 4 =============*/
+        /*============= exercise 5 =============*/
+        /*============= exercise 6 =============*/
+        /*============= exercise 7 =============*/
+        /*============= exercise 8 =============*/
+        /*============= exercise 9 =============*/
+        /*============= exercise 10 =============*/
     }
 }
